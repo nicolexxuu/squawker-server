57dc3d35db8fc76dee7a44166c2e4f660725f09f
Nicole Xu <diudiuxu@gmail.com>
2023-04-23 22:58:07 -0500

    hi

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.use(function (req, res, next) {
 
 app.use(errorHandler);
 
-app.listen(PORT, function () {
-    console.log(`Server is starting on port ${PORT}`)
+app.listen(process.env.PORT || PORT, function () {
+    // console.log(`Server is starting on port ${PORT}`)
 })
